d107ec0a28daf95d2fd4c2b2ba23a75c14b855a3
oliver.maender <oliver.maender@htwg-konstanz.de>
2023-11-21 01:00:06 +0100

    added search for admin

--- api/controllers/MovieController.js
+++ api/controllers/MovieController.js
@@ -7,63 +7,72 @@
 
 module.exports = {
 
-    create: async function (req, res) {
-        let params = req.allParams();
-        await Movie.create(params);
-        res.view('pages/movie/status', { moviename: params.name });
-    },
+  create: async function (req, res) {
+    let params = req.allParams();
+    await Movie.create(params);
+    res.view('pages/movie/status', { moviename: params.name });
+  },
 
-    find: async function (req, res) {
-        let movies;
-        if (req.query.q && req.query.q.length > 0) {
-            movies = await Movie.find({
-              name: {
-                'contains': req.query.q
-              }
-            })
-          } else {
-            movies = await Movie.find();
-          }
-          res.view ('pages/search', { movies: movies } );
-    },
+  find: async function (req, res) {
+    let movies;
+    if (req.query.q && req.query.q.length > 0) {
+      movies = await Movie.find({
+        name: {
+          'contains': req.query.q
+        }
+      })
+    } else {
+      movies = await Movie.find();
+    }
+    res.view('pages/search', { movies: movies });
+  },
 
-    findMoviesForAdmin: async function (req, res) {
-        let movies = await Movie.find();
-        res.view('pages/movie/index', { movies: movies });
-    },
+  findMoviesForAdmin: async function (req, res) {
+    let movies;
+    if (req.query.q && req.query.q.length > 0) {
+      movies = await Movie.find({
+        name: {
+          'contains': req.query.q
+        }
+      })
+    } else {
+      movies = await Movie.find();
+    }
+    res.view('pages/movie/index', { movies: movies });
+  },
 
-    findOne: async function (req, res) {
-        let movie = await Movie.findOne({ id: req.params.id });
-        res.view('pages/movie/show', { movie: movie });
-    },
+  findOne: async function (req, res) {
+    let movie = await Movie.findOne({ id: req.params.id });
+    res.view('pages/movie/show', { movie: movie });
+  },
 
-    edit: async function (req, res) {
-        let movie = await Movie.findOne({ id: req.params.id });
-        res.view('pages/movie/edit', { movie: movie });
-    },
+  edit: async function (req, res) {
+    let movie = await Movie.findOne({ id: req.params.id });
+    res.view('pages/movie/edit', { movie: movie });
+  },
 
-    update: async function (req, res) {
-        let params = req.allParams();
-        await Movie.updateOne({ id: req.params.id }).set(params);
-        res.redirect('/movie');
-    },
+  update: async function (req, res) {
+    let params = req.allParams();
+    await Movie.updateOne({ id: req.params.id }).set(params);
+    res.redirect('/movie');
+  },
 
-    destroy: async function (req, res) {
-        let movie = await Movie.destroyOne({ id: req.params.id });
-        res.redirect('/movie');
-    },
+  destroy: async function (req, res) {
+    let movie = await Movie.destroyOne({ id: req.params.id });
+    res.redirect('/movie');
+  },
 
-/*     search: async function (req, res) {
-        var sql = "SELECT movie.name, movie.duration, movie.description FROM movie WHERE movie.name LIKE '%" + req.body.name + "%'";
-
-        var rawResult = await sails.sendNativeQuery(sql);
-        
-        let entries  = [];
-        rawResult.rows.forEach(element => {
-          entries.push(element);
-        });
-        res.view('pages/result', { entries });
-     } */
+  /*     search: async function (req, res) {
+          var sql = "SELECT movie.name, movie.duration, movie.description FROM movie WHERE movie.name LIKE '%" + req.body.name + "%'";
+  
+          var rawResult = await sails.sendNativeQuery(sql);
+          
+          let entries  = [];
+          rawResult.rows.forEach(element => {
+            entries.push(element);
+          });
+          res.view('pages/result', { entries });
+       } */
 
 
 };
